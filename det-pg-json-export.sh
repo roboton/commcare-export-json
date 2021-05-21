4a0f6055b59e46af778639e369c5a3cb8d45393a
ron <roberton@gmail.com>
2021-05-21 08:29:09 +0800

    AWS S3 updates (credentials)

--- det-pg-json-export.sh
+++ det-pg-json-export.sh
@@ -3,4 +3,4 @@ commcare-export --project ${CC_PROJECT} --username ${CC_USER} --password ${CC_AP
     --auth-mode apikey --output-format sql --output postgresql://postgres:postgres@postgres/postgres \
     --query json_export.xlsx && \
     psql postgresql://postgres:postgres@postgres/postgres -f export_to_disk.sql && \
-    aws s3 cp /tmp/json_export.json ${S3_DEST}
+    aws s3 cp /tmp/json_export.json ${S3_DEST}/${CC_PROJECT}_$(date +%Y%m%dT%H%M%S).json
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$#" -ne 4 ]; then
-    echo "./run.sh <commcare username> <commcare api key> <commcare project> <s3 destination>"
+if [ "$#" -ne 6 ]; then
+    echo "./run.sh <commcare username> <commcare api key> <commcare project> <s3 destination> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>"
     exit 1
 fi
-CC_USER=$1 CC_APIKEY=$2 CC_PROJECT=$3 S3_DEST=$4 docker-compose up --build --abort-on-container-exit
+CC_USER=$1 CC_APIKEY=$2 CC_PROJECT=$3 AWS_ACCESS_KEY_ID=$4 AWS_SECRET_ACCESS_KEY=$5 S3_DEST=$6 docker-compose up --build --abort-on-container-exit
